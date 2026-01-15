d9c7600a16c2cb02bcef8ba7cdb2af8b59a840c2
Mariolucas03 <mariolucas096@gmail.com>
2026-01-16 00:26:40 +0100

    BETA_02_ARREGLO INDEX

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
-        host: true, // 🔥 ESTO ES LA CLAVE: Permite acceso desde la red (IP)
-        port: 3000, // Fijamos el puerto para no volvernos locos
+        host: true,
+        port: 3000,
     },
+    build: {
+        outDir: 'dist', // Asegura que la salida va a 'dist'
+    }
 });
